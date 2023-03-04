c6ccf8489bdafea8978e539cbcd9124f0d1c8f3e
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-04 09:19:42 +0000

    WIP

--- src/w2-10-regular-expression-matching/index.spec.ts
+++ src/w2-10-regular-expression-matching/index.spec.ts
@@ -1,7 +1,34 @@
 import { isMatch } from "./index";
 
 describe('isMatch', () => {
-    it ('checks basic strings', () => {
+    test ('plain strings', () => {
         expect(isMatch('a', 'a')).toBe(true);
+        expect(isMatch('a', 'b')).toBe(false);
+        expect(isMatch('abc', 'a')).toBe(false);
+        expect(isMatch('a', 'abc')).toBe(false);
+        expect(isMatch('abc', 'abc')).toBe(true);
+        expect(isMatch('aa', 'a')).toBe(false);
+        expect(isMatch('a', 'aa')).toBe(false);
+        expect(isMatch('aa', 'aa')).toBe(true);
+    });
+
+    test ('dot chars', () => {
+        expect(isMatch('a', '.')).toBe(true);
+        expect(isMatch('ab', '.b')).toBe(true);
+        expect(isMatch('abc', 'a.c')).toBe(true);
+        expect(isMatch('a', '..')).toBe(false);
+        expect(isMatch('abc', '.b.')).toBe(true);
+    });
+
+    test('wildcard', () => {
+        expect(isMatch('aaa', 'a*')).toBe(true);
+        expect(isMatch('aaa', 'a*a')).toBe(true);
+        expect(isMatch('aaa', 'aa*')).toBe(true);
+        expect(isMatch('aaa', 'b*a*')).toBe(true);
+        expect(isMatch('aaa', 'b*')).toBe(false);
+    });
+
+    test('cases', () => {
+        expect(isMatch('mississippi', 'mis*is*ip*.')).toBe(true);
     })
 });
--- src/w2-10-regular-expression-matching/index.ts
+++ src/w2-10-regular-expression-matching/index.ts
@@ -1,5 +1,66 @@
-console.log('Week II Task 10');
-
 export function isMatch(s: string, p: string): boolean {
+    var i = 0;
+    var pi = 0;
+
+    while (i < s.length) {
+        if (pi === p.length) {
+            return false;
+        }
+
+        if (isMatchChar(s[i], p[pi])) {
+            i++;
+            pi++;
+            continue;
+        }
+
+        const notLastP = pi < p.length - 2;
+
+        if (notLastP && p[pi + 1] == '*') {
+            pi += 2;
+            continue;
+        }
+
+        if (p[pi] == '*') {
+            if (isMatchChar(s[i], p[pi - 1])) {
+                i++;
+                continue;
+            }
+
+            if (!notLastP && isMatchChar(s[i], p[pi + 1])) {
+                pi++;
+                continue;
+            }
+        }
+
+        return false;
+    }
+
+    var pe = pi;
+
+    pi = p.length - 1;
+    i = s.length - 1;
+
+    while (pi >= pe) {
+        if (i == 0) {
+            return false;
+        }
+        if (p[pi] == '*') {
+            pi -= 2;
+            continue;
+        }
+
+        if (isMatchChar(s[i], p[pi])) {
+            pi--;
+            i--;
+            continue;
+        }
+
+        return false;
+    }
+
     return true;
 };
+
+function isMatchChar(s: string, p: string): boolean {
+    return s == p || p == '.';
+}
