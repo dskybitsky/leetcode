2706908bda97594bb08057b143e47adb3ed6cff9
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-16 09:51:36 +0000

    Solved (Slow)

--- src/106-construct-binary-tree-inorder-postorder/index.spec.ts
+++ src/106-construct-binary-tree-inorder-postorder/index.spec.ts
@@ -14,5 +14,32 @@ describe('buildTree', () => {
                     )
                 )
             );
-    })
+    });
+
+    test('case 3', () => {
+        expect(buildTree([2, 3, 1], [3, 2, 1]))
+            .toEqual(
+                new TreeNode(
+                    1,
+                    new TreeNode(
+                        2,
+                        null,
+                        new TreeNode(3)
+                    ),
+                )
+            );
+    });
+
+    test('case 4', () => {
+        expect(buildTree([3, 2, 1], [3, 2, 1]))
+            .toEqual(
+                new TreeNode(
+                    1,
+                    new TreeNode(
+                        2,
+                        new TreeNode(3)
+                    ),
+                )
+            );
+    });
 });
--- src/106-construct-binary-tree-inorder-postorder/index.ts
+++ src/106-construct-binary-tree-inorder-postorder/index.ts
@@ -11,5 +11,34 @@ export class TreeNode {
 }
 
 export function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
-    return null;
+    if (!postorder.length) {
+        return null;
+    }
+
+    const rootVal = postorder[postorder.length - 1];
+
+    const inorderSplitIndex = inorder.indexOf(rootVal);
+
+    const leftInorder = inorder.slice(0, inorderSplitIndex);
+    const rightInorder = inorder.slice(inorderSplitIndex + 1);
+
+    const leftPostorder = [];
+    const rightPostorder = [];
+
+    for (let i = postorder.length - 2; i >= 0; i--) {
+        const index = leftInorder.indexOf(postorder[i]);
+
+        if (index < 0) {
+            rightPostorder.unshift(postorder[i]);
+        } else {
+            leftPostorder.unshift(postorder[i]);
+        }
+    }
+
+    const root = new TreeNode(rootVal);
+    
+    root.left = buildTree(leftInorder, leftPostorder);
+    root.right = buildTree(rightInorder, rightPostorder);
+
+    return root;
 };
