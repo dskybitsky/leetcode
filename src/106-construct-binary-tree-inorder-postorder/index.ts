f644fc844db15d5d62a44d773d9734b1a2fb447c
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-16 10:45:58 +0000

    fix

--- src/106-construct-binary-tree-inorder-postorder/index.ts
+++ src/106-construct-binary-tree-inorder-postorder/index.ts
@@ -10,20 +10,27 @@ export class TreeNode {
     }
 }
 
-export function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
-    if (!postorder.length) {
+export function buildTree(
+    inorder: number[], 
+    postorder: number[],
+    inorderLeft = 0,
+    inorderRight = inorder.length - 1,
+    postorderLeft = 0,
+    postorderRight = postorder.length - 1
+): TreeNode | null {
+    if (postorderLeft > postorderRight) {
         return null;
     }
 
-    const rootVal = postorder[postorder.length - 1];
+    const rootVal = postorder[postorderRight];
 
     const inorderSplitIndex = inorder.indexOf(rootVal);
 
     const leftInorder = inorder.slice(0, inorderSplitIndex);
     const rightInorder = inorder.slice(inorderSplitIndex + 1);
 
-    const leftPostorder = postorder.slice(0, leftInorder.length);
-    const rightPostorder = postorder.slice(leftInorder.length, postorder.length - 1);
+    const leftPostorder = postorder.slice(0, inorderSplitIndex);
+    const rightPostorder = postorder.slice(inorderSplitIndex, postorder.length - 1);
 
     const root = new TreeNode(rootVal);
     
