794863ebaa95b7d7b6d2b0ae158dfd1d7cc6b355
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-20 08:16:27 +0000

    4 Partial

--- src/4-median-of-two-sorted-arrays/index.spec.ts
+++ src/4-median-of-two-sorted-arrays/index.spec.ts
@@ -2,10 +2,35 @@ import { findMedianSortedArrays } from './index';
 
 describe('findMedianSortedArrays', () => {
     test('case 1', () => {
-        expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
+        expect(findMedianSortedArrays([], [])).toBe(0);
     });
 
     test('case 2', () => {
-        expect(findMedianSortedArrays([1, 3], [2, 4])).toBe(2.5);
+        expect(findMedianSortedArrays([1, 2], [])).toBe(1.5);
+        expect(findMedianSortedArrays([1, 2, 3], [])).toBe(2);
     });
+
+    test('case 3', () => {
+        expect(findMedianSortedArrays([], [3, 4])).toBe(3.5);
+        expect(findMedianSortedArrays([], [3, 4, 5])).toBe(4);
+    });
+
+    test('case 4', () => {
+        expect(findMedianSortedArrays([1], [2, 3])).toBe(2);
+        expect(findMedianSortedArrays([1], [2, 3, 4])).toBe(2.5);
+
+        expect(findMedianSortedArrays([1, 2], [3])).toBe(2);
+        expect(findMedianSortedArrays([1, 2, 3], [4])).toBe(2.5);
+
+        expect(findMedianSortedArrays([1, 2], [3, 4])).toBe(2.5);
+        expect(findMedianSortedArrays([3, 4], [1, 2])).toBe(2.5);
+    });
+
+    // test('case 1', () => {
+    //     expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
+    // });
+
+    // test('case 2', () => {
+    //     expect(findMedianSortedArrays([1, 3], [2, 4])).toBe(2.5);
+    // });
 });
--- src/4-median-of-two-sorted-arrays/index.ts
+++ src/4-median-of-two-sorted-arrays/index.ts
@@ -1,4 +1,50 @@
 export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    return 1;
+    if (nums1.length === 0) {
+        return findMedianSortedArray(nums2);
+    }
+
+    if (nums2.length === 0) {
+        return findMedianSortedArray(nums1);
+    }
+
+    const overlapping = nums1[0] >= nums2[0] && nums1[0] <= nums2[nums2.length - 1]
+        || nums1[nums1.length - 1] >= nums2[0] && nums1[nums1.length - 1] <= nums2[nums2.length - 1];
+
+    if (!overlapping) {
+        return findMedianSortedNonOverlappingArrays(nums1, nums2);
+    }
+
+    return 0;
 };
 
+function findMedianSortedNonOverlappingArrays(nums1: number[], nums2: number[]): number {
+    const [leftNums, rightNums] = nums1[0] < nums2[0] ? [nums1, nums2] : [nums2, nums1];
+
+    if (leftNums.length < rightNums.length) {
+        return findMedianSortedArray(rightNums, (-1) * leftNums.length);
+    }
+
+    if (leftNums.length > rightNums.length) {
+        return findMedianSortedArray(leftNums, rightNums.length);
+    }
+
+    return (leftNums[leftNums.length - 1] + rightNums[0]) / 2;
+}
+
+function findMedianSortedArray(nums: number[], offset = 0): number {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    const length = nums.length + Math.abs(offset);
+
+    const mid = offset > 0 
+        ? Math.floor(length / 2) 
+        : Math.floor(length / 2) + offset;
+
+    if (length % 2 === 1) {
+        return nums[mid];
+    }
+
+    return (nums[mid] + nums[mid - 1]) / 2;
+}
