eb8760d44acd6db719213b8edb0077a70e00a57c
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-21 08:08:15 +0000

    4 Solved (kinda)

--- src/4-median-of-two-sorted-arrays/index.spec.ts
+++ src/4-median-of-two-sorted-arrays/index.spec.ts
@@ -2,35 +2,33 @@ import { findMedianSortedArrays } from './index';
 
 describe('findMedianSortedArrays', () => {
     test('case 1', () => {
-        expect(findMedianSortedArrays([], [])).toBe(0);
+        expect(findMedianSortedArrays([], [])).toBe(-1);
+        expect(findMedianSortedArrays([], [1])).toBe(1);
+        expect(findMedianSortedArrays([], [1, 2])).toBe(1.5);
+        expect(findMedianSortedArrays([], [1, 2, 3])).toBe(2);
     });
 
     test('case 2', () => {
-        expect(findMedianSortedArrays([1, 2], [])).toBe(1.5);
-        expect(findMedianSortedArrays([1, 2, 3], [])).toBe(2);
+        expect(findMedianSortedArrays([1], [2])).toBe(1.5);
+        expect(findMedianSortedArrays([1], [2, 3])).toBe(2);
+        expect(findMedianSortedArrays([1], [1, 2, 3])).toBe(1.5);
     });
 
     test('case 3', () => {
-        expect(findMedianSortedArrays([], [3, 4])).toBe(3.5);
-        expect(findMedianSortedArrays([], [3, 4, 5])).toBe(4);
+        expect(findMedianSortedArrays([1, 2], [3, 4])).toBe(2.5);
+        expect(findMedianSortedArrays([1, 2], [3, 4, 5])).toBe(3);
+        expect(findMedianSortedArrays([1, 2], [3, 4, 5, 6])).toBe(3.5);
+        expect(findMedianSortedArrays([2, 3], [4, 5, 6, 7])).toBe(4.5);
     });
 
     test('case 4', () => {
-        expect(findMedianSortedArrays([1], [2, 3])).toBe(2);
-        expect(findMedianSortedArrays([1], [2, 3, 4])).toBe(2.5);
-
-        expect(findMedianSortedArrays([1, 2], [3])).toBe(2);
-        expect(findMedianSortedArrays([1, 2, 3], [4])).toBe(2.5);
-
-        expect(findMedianSortedArrays([1, 2], [3, 4])).toBe(2.5);
-        expect(findMedianSortedArrays([3, 4], [1, 2])).toBe(2.5);
+        expect(findMedianSortedArrays([1, 2, 3], [3, 4, 5])).toBe(3);
+        expect(findMedianSortedArrays([1, 2, 3, 4], [5, 6, 7])).toBe(4);
+        expect(findMedianSortedArrays([1, 2, 3], [4, 5, 6, 7])).toBe(4);
+        expect(findMedianSortedArrays([1, 2, 3, 4], [5, 6, 7, 8])).toBe(4.5);
+        expect(findMedianSortedArrays([1, 2, 3], [4, 5, 6, 7, 8])).toBe(4.5);
+        expect(findMedianSortedArrays([1, 5, 6], [2, 3, 4, 7, 8])).toBe(4.5);
+        expect(findMedianSortedArrays([2, 2, 4, 4], [2, 2, 4, 4])).toBe(3);
+        expect(findMedianSortedArrays([1, 4, 5, 6], [2, 3, 7, 8])).toBe(4.5);
     });
-
-    // test('case 1', () => {
-    //     expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
-    // });
-
-    // test('case 2', () => {
-    //     expect(findMedianSortedArrays([1, 3], [2, 4])).toBe(2.5);
-    // });
 });
--- src/4-median-of-two-sorted-arrays/index.ts
+++ src/4-median-of-two-sorted-arrays/index.ts
@@ -1,50 +1,98 @@
 export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    if (nums1.length === 0) {
-        return findMedianSortedArray(nums2);
-    }
+    const [nums1st, nums2nd] = nums1.length <= nums2.length 
+        ? [nums1, nums2]
+        : [nums2, nums1];
 
-    if (nums2.length === 0) {
-        return findMedianSortedArray(nums1);
-    }
-
-    const overlapping = nums1[0] >= nums2[0] && nums1[0] <= nums2[nums2.length - 1]
-        || nums1[nums1.length - 1] >= nums2[0] && nums1[nums1.length - 1] <= nums2[nums2.length - 1];
-
-    if (!overlapping) {
-        return findMedianSortedNonOverlappingArrays(nums1, nums2);
-    }
-
-    return 0;
-};
-
-function findMedianSortedNonOverlappingArrays(nums1: number[], nums2: number[]): number {
-    const [leftNums, rightNums] = nums1[0] < nums2[0] ? [nums1, nums2] : [nums2, nums1];
-
-    if (leftNums.length < rightNums.length) {
-        return findMedianSortedArray(rightNums, (-1) * leftNums.length);
-    }
-
-    if (leftNums.length > rightNums.length) {
-        return findMedianSortedArray(leftNums, rightNums.length);
-    }
-
-    return (leftNums[leftNums.length - 1] + rightNums[0]) / 2;
+    return findMedianOrderedSortedArrays(nums1st, nums2nd);
 }
 
-function findMedianSortedArray(nums: number[], offset = 0): number {
-    if (nums.length === 0) {
-        return 0;
+function findMedianOrderedSortedArrays(nums1: number[], nums2: number[]): number {
+    if (nums1.length === 0) {
+        return findMedianArray(nums2);
     }
 
-    const length = nums.length + Math.abs(offset);
+    const mid2 = Math.floor(nums2.length / 2);
 
-    const mid = offset > 0 
-        ? Math.floor(length / 2) 
-        : Math.floor(length / 2) + offset;
+    if (nums1.length === 1) {
+        if (nums2.length === 1) {
+            return findMedian2(nums1[0], nums2[0]);
+        }
 
-    if (length % 2 === 1) {
+        if (nums2.length % 2 === 1) {
+            return findMedian4(nums1[0], nums2[mid2 - 1], nums2[mid2], nums2[mid2 + 1]);
+        }
+
+        return findMedian3(nums1[0], nums2[mid2 - 1], nums2[mid2]);
+    }
+
+    if (nums1.length === 2) {
+        if (nums2.length === 2) {
+            return findMedian4(nums1[0], nums1[1], nums2[0], nums2[1]);
+        }
+
+        if (nums2.length % 2 === 1) {
+            return findMedian3(
+                nums2[mid2],
+                Math.max(nums1[1], nums2[mid2 - 1]),
+                Math.min(nums1[0], nums2[mid2 + 1])
+            );
+        }
+
+        return findMedian4(
+            nums2[mid2 - 1],
+            nums2[mid2],
+            Math.max(nums1[0], nums2[mid2 - 2]),
+            Math.min(nums1[1], nums2[mid2 + 1])
+        );
+    }
+
+    const idx1 = Math.floor((nums1.length - 1) / 2);
+    const idx2 = Math.floor((nums2.length - 1) / 2);
+
+    if(nums1[idx1] <= nums2[idx2]) {
+        return findMedianOrderedSortedArrays(
+            nums1.slice(idx1),
+            nums2.slice(0, nums2.length - idx1)
+        );
+    }
+
+    return findMedianOrderedSortedArrays(
+        nums1.slice(0, nums1.length - idx1),
+        nums2.slice(idx1)
+    );
+};
+
+function findMedian2(num1: number, num2: number): number {
+    return (num1 + num2) / 2;
+}
+
+function findMedian3(num1: number, num2: number, num3: number): number {
+    return (
+        num1 + num2 + num3 
+        - Math.min(num1, num2, num3)
+        - Math.max(num1, num2, num3)
+    );
+}
+
+function findMedian4(num1: number, num2: number, num3: number, num4: number): number {
+    return (
+        num1 + num2 + num3 + num4 
+        - Math.min(num1, num2, num3, num4)
+        - Math.max(num1, num2, num3, num4)
+    ) / 2;
+}
+
+function findMedianArray(nums: number[]): number {
+    if (nums.length === 0) {
+        return -1;
+    }
+
+    const mid = Math.floor(nums.length / 2);
+
+    if (nums.length % 2 === 1) {
         return nums[mid];
     }
 
     return (nums[mid] + nums[mid - 1]) / 2;
 }
+    
