26e10532aa855424516260d34370e55842a0b7b2
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-23 04:58:13 +0000

    1319 - Base

---
+++ src/1319-number-of-operations/index.spec.ts
@@ -0,0 +1,18 @@
+import { makeConnected } from "./index";
+
+describe('makeConnected', () => {
+    test('case 1', () => {
+        expect(makeConnected(4, [[0, 1], [0, 2], [1, 2]]))
+            .toBe(1);
+    });
+
+    test('case 2', () => {
+        expect(makeConnected(6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
+            .toBe(2);
+    });
+
+    test('case 3', () => {
+        expect(makeConnected(6, [[0, 1], [0, 2], [0, 3], [1, 2]]))
+            .toBe(-1);
+    });
+});
