cb56dd54d91eff1821474f4207c6fa1fce18970f
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-24 09:21:27 +0000

    29 Base

---
+++ src/29-divide-two-integers/index.spec.ts
@@ -0,0 +1,15 @@
+import { divide } from "./index";
+
+describe('divide', () => {
+    test('case 1', () => {
+        expect(divide(10, 2)).toBe(3);
+    });
+
+    test('case 2', () => {
+        expect(divide(7, -3)).toBe(-2);
+    });
+
+    test('case 3', () => {
+        expect(divide(-2147483648, -1)).toBe(2147483647);
+    });
+});
