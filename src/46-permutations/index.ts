ae7842c15f8abce95c379cb9d6e2482056bbed19
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-25 19:37:47 +0000

    46 Base

---
+++ src/46-permutations/index.ts
@@ -0,0 +1,11 @@
+export function permute(nums: number[]): number[][] {
+    if (nums.length === 0) {
+        return [];
+    }
+
+    if (nums.length === 1) {
+        return [nums];
+    }
+
+    
+};
