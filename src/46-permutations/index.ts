a92a24dbb95fa1dfb4e60cba7e480890dd7982eb
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-26 04:22:26 +0000

    46 Solved

--- src/46-permutations/index.ts
+++ src/46-permutations/index.ts
@@ -1,11 +1,27 @@
-export function permute(nums: number[]): number[][] {
-    if (nums.length === 0) {
+export function permute(nums: number[], from = 0): number[][] {
+    if (from == nums.length) {
         return [];
     }
 
-    if (nums.length === 1) {
-        return [nums];
+    if (from === nums.length - 1) {
+        return [[nums[from]]];
     }
 
-    
+    const result: number[][] = [];
+
+    const nextPermutes = permute(nums, from + 1);
+
+    for (let i = 0; i < nextPermutes.length; i++) {
+        for (let j = 0; j <= nextPermutes[i].length; j++) {
+            result.push(insert(nextPermutes[i], j, nums[from]));
+        }
+    }
+
+    return result;
 };
+
+const insert = <T>(arr: T[], index: number, newItem: T) => [
+    ...arr.slice(0, index),
+    newItem,
+    ...arr.slice(index)
+]
