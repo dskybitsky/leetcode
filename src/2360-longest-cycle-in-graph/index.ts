ec419ab27b9ca5567f4af3c18d342c067b79490b
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-26 05:10:36 +0000

    2360 Solved (slow)

--- src/2360-longest-cycle-in-graph/index.ts
+++ src/2360-longest-cycle-in-graph/index.ts
@@ -1,3 +1,62 @@
 export function longestCycle(edges: number[]): number {
-    return -1;
+    const vertices = new Set<number>();
+
+    for (let i = 0; i < edges.length; i++) {
+        vertices.add(i);
+    }
+
+    let result = -1;
+
+    const adjMap = buildAdjMap(edges);
+
+    while (vertices.size > 0) {
+        const [v] = vertices;
+
+        const vertsToVisit = [v];
+        const vertsVisited = new Set<number>();
+
+        vertsVisited.add(v);
+
+        let cycleLen = 1;
+
+        while (vertsToVisit.length > 0) {
+            const v1 = vertsToVisit.shift();
+        
+            if (adjMap.has(v1)) {
+                for (const v2 of adjMap.get(v1)) {
+                    if (!vertsVisited.has(v2)) {
+                        vertsToVisit.push(v2);
+                        vertsVisited.add(v2);
+                        cycleLen++;
+                    } else if (v2 === v) {
+                        result = Math.max(result, cycleLen);
+                        cycleLen = 1;
+                    }
+                }
+            }
+        }
+
+        vertices.delete(v);
+    }
+
+    return result;
+};
+
+function buildAdjMap(edges: number[]): Map<number, Set<number>> {
+    const map = new Map<number, Set<number>>();
+
+    for (let i = 0; i < edges.length; i++) {
+        if (edges[i] >= 0) {
+            const v1 = i;
+            const v2 = edges[i];
+
+            if (!map.has(v1)) {
+                map.set(v1, new Set<number>());
+            }
+    
+            map.get(v1).add(v2);
+        }
+    }
+
+    return map;
 };
