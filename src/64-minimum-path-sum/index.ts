cdee6a61d9ce3a5c068f378c0cc8a3113f27de15
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-27 05:34:05 +0000

    64 Solved - Slow

--- src/64-minimum-path-sum/index.ts
+++ src/64-minimum-path-sum/index.ts
@@ -1,3 +1,12 @@
-export function minPathSum(grid: number[][]): number {
+export function minPathSum(grid: number[][], node = [0, 0], sum = 0): number {
+    const [nodeI, nodeJ] = node;
 
+    if (nodeI === grid.length - 1 && nodeJ === grid[nodeI].length - 1) {
+        return sum + grid[nodeI][nodeJ];
+    }
+
+    return Math.min(
+        nodeI < grid.length - 1 ? minPathSum(grid, [nodeI + 1, nodeJ], sum + grid[nodeI][nodeJ]) : 200 * 200 * 100,
+        nodeJ < grid[nodeI].length - 1 ? minPathSum(grid, [nodeI, nodeJ + 1], sum + grid[nodeI][nodeJ]) : 200 * 200 * 100,
+    );
 };
