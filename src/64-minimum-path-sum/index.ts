cfa1fafc61c4664fe4c5087a65d774533c22cfab
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-27 05:48:53 +0000

    64 Solved

--- src/64-minimum-path-sum/index.ts
+++ src/64-minimum-path-sum/index.ts
@@ -1,12 +1,21 @@
-export function minPathSum(grid: number[][], node = [0, 0], sum = 0): number {
-    const [nodeI, nodeJ] = node;
+export function minPathSum(grid: number[][]): number {
+    const dp: number[][] = [];
 
-    if (nodeI === grid.length - 1 && nodeJ === grid[nodeI].length - 1) {
-        return sum + grid[nodeI][nodeJ];
+    for (let i = 0; i < grid.length; i++) {
+        dp[i] = [];
+
+        for (let j = 0; j < grid[i].length; j++) {
+            if (i === 0 && j === 0) {
+                dp[i][j] = grid[i][j];
+            } else if (i === 0) {
+                dp[i][j] = grid[i][j] + dp[i][j - 1];
+            } else if (j === 0) {
+                dp[i][j] = grid[i][j] + dp[i - 1][j];
+            } else {
+                dp[i][j] = grid[i][j] + Math.min(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
     }
 
-    return Math.min(
-        nodeI < grid.length - 1 ? minPathSum(grid, [nodeI + 1, nodeJ], sum + grid[nodeI][nodeJ]) : 200 * 200 * 100,
-        nodeJ < grid[nodeI].length - 1 ? minPathSum(grid, [nodeI, nodeJ + 1], sum + grid[nodeI][nodeJ]) : 200 * 200 * 100,
-    );
+    return dp[grid.length - 1][grid[grid.length - 1].length - 1];
 };
