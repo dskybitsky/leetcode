9295bcb95a7660c1b666758ee7bf4349809b9ee3
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-27 08:56:51 +0000

    42 Basis

---
+++ src/42-trapping-rain-water/index.spec.ts
@@ -0,0 +1,13 @@
+import { trap } from "./index";
+
+describe('trap', () => {
+    test('case 1', () => {
+        expect(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+            .toBe(6);
+    });
+
+    test('case 2', () => {
+        expect(trap([4, 2, 0, 3, 2, 5]))
+            .toBe(9);
+    });
+});
