23da179d3fd68cb0a09b7a021a4fa403d6f54298
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-27 08:54:46 +0000

    25 Solved

--- src/25-reverse-nodes-in-k-group/index.spec.ts
+++ src/25-reverse-nodes-in-k-group/index.spec.ts
@@ -1,4 +1,4 @@
-import { ListNode, reverseKGroup } from "./index";
+import { ListNode, reverseKGroup, reverseList } from "./index";
 
 describe('reverseKGroup', () => {
     test('case 1', () => {
@@ -10,12 +10,41 @@ describe('reverseKGroup', () => {
         )
     });
 
-    test('case 1', () => {
+    test('case 2', () => {
         expect(reverseKGroup(
             new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))),
             3
         )).toEqual(
             new ListNode(3, new ListNode(2, new ListNode(1, new ListNode(4, new ListNode(5))))),
         )
+    });
+
+    test('case 3', () => {
+        expect(reverseKGroup(
+            new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6)))))),
+            2
+        )).toEqual(
+            new ListNode(2, new ListNode(1, new ListNode(4, new ListNode(3, new ListNode(6, new ListNode(5)))))),
+        )
+    });
+});
+
+describe('reverseList', () => {
+    test('case 1', () => {
+        const head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        expect(reverseList(head, head.next.next))
+            .toEqual(
+                new ListNode(3, new ListNode(2, new ListNode(1, new ListNode(4, new ListNode(5))))),
+            );
+    });
+
+    test('case 2', () => {
+        const head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        expect(reverseList(head, head.next))
+            .toEqual(
+                new ListNode(2, new ListNode(1, new ListNode(3, new ListNode(4, new ListNode(5))))),
+            );
     });
 });
--- src/25-reverse-nodes-in-k-group/index.ts
+++ src/25-reverse-nodes-in-k-group/index.ts
@@ -9,5 +9,52 @@ export class ListNode {
 }
 
 export function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
-    return null;
+    let newHead: ListNode | null = null;
+    let oldTail: ListNode | null = null;
+
+    let slow = head;
+    let fast = slow;
+
+    while (fast) {
+        let i = 1;
+
+        while (fast && i < k) {
+            i++;
+            fast = fast.next;
+        }
+
+        if (!fast) {
+            break;
+        }
+        
+        const next = fast.next;
+
+        const reverseHead = reverseList(slow, fast);
+
+        if (newHead === null) {
+            newHead = reverseHead;
+        } else {
+            oldTail.next = reverseHead;
+        }
+
+        oldTail = slow;
+
+        slow = next;
+        fast = slow;
+    }
+
+    return newHead;
 };
+
+export function reverseList(head: ListNode, tail: ListNode): ListNode {
+    if (head === tail) {
+        return head;
+    }
+
+    const nextHead = head.next;
+
+    head.next = tail.next;
+    tail.next = head;
+
+    return reverseList(nextHead, tail);
+}
