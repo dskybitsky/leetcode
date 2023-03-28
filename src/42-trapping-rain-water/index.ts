6f7d52e6b46647ea8a69a1091210525bfd673114
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-28 10:11:14 +0000

    42 Wrong

--- src/42-trapping-rain-water/index.ts
+++ src/42-trapping-rain-water/index.ts
@@ -1,11 +1,35 @@
 export function trap(height: number[]): number {
-    let lastPeak: number | null = null;
+    let result = 0;
 
-    for (let i = 1; i < height.length; i++) {
-        if (height[i - 1] > height[i]) {
-            if (lastPeak === null) {
-                lastPeak = i - 1;
-            }
+    let p1 = 0;
+    let p2 = 0;
+
+    while (p1 < height.length) {
+        while (p1 < height.length - 1 && height[p1 + 1] >= height[p1]) {
+            p1++;
         }
+
+        if (p1 === height.length - 1) {
+            return result;
+        }
+
+        let interResult = 0;
+
+        p2 = p1 + 1;
+
+        while (p2 < height.length && height[p2] < height[p1]) {
+            interResult += height[p1] - height[p2];
+            p2++;
+        }
+
+        if (p2 === height.length) {
+            return result;
+        }
+
+        result += interResult;
+
+        p1 = p2;
     }
+
+    return result;
 };
