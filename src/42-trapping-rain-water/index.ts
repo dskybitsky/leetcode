e13ec91e360374772066c1c772d80f116f4088fd
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-28 03:39:51 +0000

    42 Basis

--- src/42-trapping-rain-water/index.ts
+++ src/42-trapping-rain-water/index.ts
@@ -1,3 +1,11 @@
 export function trap(height: number[]): number {
-    return 0;
+    let lastPeak: number | null = null;
+
+    for (let i = 1; i < height.length; i++) {
+        if (height[i - 1] > height[i]) {
+            if (lastPeak === null) {
+                lastPeak = i - 1;
+            }
+        }
+    }
 };
