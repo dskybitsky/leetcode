997e02b5edc34bc5395f9d1fb911d2c59215ec6b
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-03-28 09:42:28 +0000

    983 Solved

--- src/983-minimum-cost-for-tickets/index.spec.ts
+++ src/983-minimum-cost-for-tickets/index.spec.ts
@@ -5,4 +5,11 @@ describe('mincostTickets', () => {
         expect(mincostTickets([1, 4, 6, 7, 8, 20], [2, 7, 15]))
             .toBe(11);
     });
+
+    test('case 2', () => {
+        expect(mincostTickets(
+            [1, 4, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 27, 28],
+            [3, 13, 45]
+        )).toBe(44);
+    });
 });
--- src/983-minimum-cost-for-tickets/index.ts
+++ src/983-minimum-cost-for-tickets/index.ts
@@ -1,3 +1,42 @@
-export function mincostTickets(days: number[], costs: number[]): number {
-    return 0;
+let memo = [];
+
+export function mincostTickets(days: number[], costs: number[], offset = 0): number {
+    if (offset === 0) {
+        memo = [];
+    }
+
+    if (memo[offset]) {
+        return memo[offset];
+    }
+
+    if (offset >= days.length) {
+        return 0;
+    }
+
+    const nextOffset1 = getNextOffset(1, days, offset);
+    const nextOffset7 = getNextOffset(7, days, offset);
+    const nextOffset30 = getNextOffset(30, days, offset);
+
+    const result = Math.min(
+        costs[0] + mincostTickets(days, costs, nextOffset1),
+        costs[1] + mincostTickets(days, costs, nextOffset7),
+        costs[2] + mincostTickets(days, costs, nextOffset30)
+    );
+
+    memo[offset] = result;
+
+    return result;
 };
+
+function getNextOffset(daysPaid: number, days: number[], offset = 0): number {
+    let i = offset;
+
+    while (
+        i < days.length 
+        && (days[i] - days[offset] + 1 <= daysPaid)
+    ) {
+        i++;
+    }
+
+    return i;
+}
