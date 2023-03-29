cd4b229b69d2fb15f07bb711670c732c3ebcefd7
Dmitrii Skybitsky <dskybitsky@gmail.com>
2023-03-29 03:53:47 +0000

    1402 Solved Slow

--- src/1402-reducing-dishes/index.ts
+++ src/1402-reducing-dishes/index.ts
@@ -1,3 +1,20 @@
-export function maxSatisfaction(satisfaction: number[]): number {
-    return 0;
+export function maxSatisfaction(satisfaction: number[], from = 0): number {
+    if (from > satisfaction.length - 1) {
+        return 0;
+    }
+
+    satisfaction.sort((a, b) => a - b);
+
+    return dp(satisfaction);
 };
+
+function dp(sat: number[], from = 0, q = 1): number {
+    if (from > sat.length - 1) {
+        return 0;
+    }
+
+    return Math.max(
+        sat[from] * q + dp(sat, from + 1, q + 1),
+        dp(sat, from + 1, q)
+    );
+}
