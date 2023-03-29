eae49f439e76f8a0fd4e76e5890fce8f324f06d6
Dmitrii Skybitsky <dskybitsky@gmail.com>
2023-03-29 04:07:45 +0000

    1402 Solved

--- src/1402-reducing-dishes/index.ts
+++ src/1402-reducing-dishes/index.ts
@@ -1,36 +1,17 @@
-let memo: number[][] = [];
-
 export function maxSatisfaction(satisfaction: number[], from = 0): number {
-    memo = [];
-
-    for (let i = 0; i < satisfaction.length; i++) {
-        memo[i] = [];
-    }
-
-    if (from > satisfaction.length - 1) {
-        return 0;
-    }
-
     satisfaction.sort((a, b) => a - b);
 
-    return dp(satisfaction);
-};
+    let result = 0;
+    let suffixResult = 0;
 
-function dp(sat: number[], from = 0, q = 1): number {
-    if (from > sat.length - 1) {
-        return 0;
+    for (let i = satisfaction.length - 1; i >= 0; i--) {
+        if (suffixResult + satisfaction[i] < 0)  {
+            return result;
+        }
+
+        suffixResult += satisfaction[i];
+        result += suffixResult;
     }
-
-    if (memo[from][q] !== undefined) {
-        return memo[from][q];
-    }
-
-    const result = Math.max(
-        sat[from] * q + dp(sat, from + 1, q + 1),
-        dp(sat, from + 1, q)
-    );
-
-    memo[from][q] = result;
 
     return result;
-}
+};
