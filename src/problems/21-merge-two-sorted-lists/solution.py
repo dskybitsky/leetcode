f62dcc5fbc3f219a155f404f8f92b9349fab9480
Skybitsky <dskybitsky@gmail.com>
2023-03-30 06:14:54 +0300

    Project restructuring

---
+++ src/problems/21-merge-two-sorted-lists/solution.py
@@ -0,0 +1,16 @@
+import sys
+from typing import Optional
+ 
+sys.path.append('../../structures')
+
+from structures import ListNode
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: 
+        return list1
+
+solution = Solution()
+
+newList = solution.mergeTwoLists(ListNode(1), ListNode(2))
+
+print(newList)
