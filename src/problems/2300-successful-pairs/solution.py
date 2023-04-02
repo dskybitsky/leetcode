db7d581149afa31d2878650981d65998f03607e8
Dmitrii Skybitsky <dskybitsky@gmail.com>
2023-04-02 04:55:16 +0000

    2300 Solved with bisect

--- src/problems/2300-successful-pairs/solution.py
+++ src/problems/2300-successful-pairs/solution.py
@@ -10,6 +10,7 @@ sys.path.append(root)
 from typing import List
 from functools import cache
 from math import floor
+from bisect import bisect_left
 
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
@@ -22,62 +23,17 @@ class Solution:
         for i in range(len(spells)):
             target = success / spells[i];
 
-            firstSuccPotion = self.findFirstGreaterOrEqual(potions, target)
+            firstSuccPotion = bisect_left(potions, target)
 
             ans.append(totalPotions - firstSuccPotion if firstSuccPotion >= 0 else 0)
 
         return ans
-
-    def findFirstGreaterOrEqual(self, nums: List[int], target: float) -> int:
-        left = 0
-        right = len(nums) - 1
-
-        if right < 0:
-            return -1
-
-        while (left <= right):
-            if left == right:
-                return left if nums[left] >= target else -1
-
-            mid = left + floor((right - left) / 2)
-
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                if mid == 0 or nums[mid - 1] < target:
-                    return mid
-                else:
-                    right = mid - 1
-        return -1
-
 
 ###############################################################################
 
 import unittest
 
 class SolutionTest(unittest.TestCase):
-    def test_findFirstGreaterOrEqual(self):
-        solution = Solution()
-        
-        self.assertEqual(solution.findFirstGreaterOrEqual([], 0), -1)
-
-        self.assertEqual(solution.findFirstGreaterOrEqual([1], 0), 0)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1], 0.5), 0)
-
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2], 0), 0)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2], 0.1), 0)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2], 1), 0)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2], 1.5), 1)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2], 2.5), -1)
-
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2, 3], 0), 0)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2, 3], 0.5), 0)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2, 3], 1), 0)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2, 3], 1.5), 1)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2, 3], 2.5), 2)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2, 3], 3), 2)
-        self.assertEqual(solution.findFirstGreaterOrEqual([1, 2, 3], 3.1), -1)
-
     def test_successfulPairs(self):
         solution = Solution()
 
