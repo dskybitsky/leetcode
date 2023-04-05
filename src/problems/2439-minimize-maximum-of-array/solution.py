c39bf319efe2cf2a6f0973e2819085bfb1fc8830
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-04-05 07:11:04 +0000

    2439 Partial solution

--- src/problems/2439-minimize-maximum-of-array/solution.py
+++ src/problems/2439-minimize-maximum-of-array/solution.py
@@ -8,10 +8,25 @@ sys.path.append(root)
 ###############################################################################
 
 from typing import List
+from math import ceil
 
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        return 0
+        max = nums[0]
+        
+        for i in range(1, len(nums)):
+            prev = nums[i  - 1]
+            curr = nums[i]
+
+            if prev > curr:
+                continue
+
+            min = ceil((curr + prev) / 2)
+
+            if min > max:
+                max = min
+
+        return max
 
 ###############################################################################
 
@@ -20,6 +35,8 @@ import unittest
 class SolutionTest(unittest.TestCase):
     def test_minimizeArrayValue(self):
         solution = Solution()
+
+        self.assertEqual(solution.minimizeArrayValue([13,13,20,0,8,9,9]), 16)
 
         self.assertEqual(solution.minimizeArrayValue([3,7,1,6]), 5)
         self.assertEqual(solution.minimizeArrayValue([10, 1]), 10)
