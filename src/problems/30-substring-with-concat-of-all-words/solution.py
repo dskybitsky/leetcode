fb8154cd3d04bfda83ff29ba54d0eb3687251b63
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-04-05 06:41:55 +0000

    30 Solved Fast

--- src/problems/30-substring-with-concat-of-all-words/solution.py
+++ src/problems/30-substring-with-concat-of-all-words/solution.py
@@ -11,12 +11,11 @@ from typing import List
 
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        words_map = {}
+        words_map = self.create_words_map(words)
+
         words_letters_map = {}
 
         for word in words:
-            words_map[word] = words_map[word] + 1 if word in words_map else 1
-            
             for letter in word:
                 words_letters_map[letter] = (
                     words_letters_map[letter] + 1 if letter in words_letters_map else 1
@@ -27,6 +26,9 @@ class Solution:
 
         chunk_letters_map = {}
 
+        if len(s)< words_len:
+            return []
+
         for i in range(0, words_len):
             chunk_letters_map[s[i]] = (
                 chunk_letters_map[s[i]] + 1 if s[i] in chunk_letters_map else 1
@@ -34,33 +36,51 @@ class Solution:
 
         ans = []
 
-        p = 1
+        p = 0
 
         while p < len(s) - words_len:
             if chunk_letters_map == words_letters_map:
-                chunk_words = [ s[i:i+word_len] for i in range(p - 1, words_len, word_len) ]
+                chunk_words_map = self.create_words_map(
+                    self.split_string(s[p:p + words_len], word_len)
+                )
 
-                chunk_words_map = {}
+                if chunk_words_map == words_map:
+                    ans.append(p)
 
-                for chunk_word in chunk_words:
-                    chunk_words_map[chunk_word] = chunk_words_map[chunk_word] + 1 if chunk_word in chunk_words_map else 1
+            chunk_letters_map[s[p]] -= 1
 
-                if (chunk_words_map == words_map):
-                    ans.append(p - 1)
+            if chunk_letters_map[s[p]] == 0:
+                chunk_letters_map.pop(s[p])
 
-            chunk_letters_map[s[p - 1]] -= 1
+            next_letter = s[p + words_len]
 
-            if chunk_letters_map[s[p - 1]] == 0:
-                chunk_letters_map.pop(s[p - 1])
-            
-            chunk_letters_map[s[p + words_len - 1]] = (
-                chunk_letters_map[s[p + words_len - 1]] + 1
-                if s[p + words_len - 1] in chunk_letters_map else 1
-            )                               
+            chunk_letters_map[next_letter] = (
+                chunk_letters_map[next_letter] + 1 if next_letter in chunk_letters_map else 1
+            )   
 
             p += 1
 
+        if chunk_letters_map == words_letters_map:
+            chunk_words_map = self.create_words_map(
+                self.split_string(s[p:p + words_len], word_len)
+            )
+
+            if chunk_words_map == words_map:
+                ans.append(p)
+
         return ans
+    
+    def split_string(self, s: str, chunk_size: int) -> List[str]:
+        return [s[i:i+chunk_size] for i in range(0, len(s), chunk_size)]
+    
+    def create_words_map(self, words: List[str]) -> dict[str, int]:
+        words_map = {}
+
+        for word in words:
+            words_map[word] = words_map[word] + 1 if word in words_map else 1
+
+        return words_map
+
 
 ###############################################################################
 
@@ -70,13 +90,17 @@ class SolutionTest(unittest.TestCase):
     def test_findSubstring(self):
         solution = Solution()
 
-        # self.assertEqual(
-        #     solution.findSubstring(
-        #         "lingmindraboofooowingdingbarrwingmonkeypoundcake", 
-        #         ["fooo","barr","wing","ding","wing"]
-        #     ),
-        #     [13]
-        # )
+        self.assertEqual(solution.findSubstring("a", ["a","a"]), [])
+
+        self.assertEqual(solution.findSubstring("wordgoodgoodgoodbestword", ["word","good","best","good"]), [8])
+
+        self.assertEqual(
+            solution.findSubstring(
+                "lingmindraboofooowingdingbarrwingmonkeypoundcake", 
+                ["fooo","barr","wing","ding","wing"]
+            ),
+            [13]
+        )
 
         self.assertEqual(solution.findSubstring("barfoothefoobarman", ["foo","bar"]), [0, 9])
         self.assertEqual(solution.findSubstring("wordgoodgoodgoodbestword", ["word","good","best","word"]), [])
