2c95f34e7a75676f3a061860b6be362076e293db
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-04-06 03:34:09 +0000

    1254 Solved

--- src/problems/1254-number-of-closed-islands/solution.py
+++ src/problems/1254-number-of-closed-islands/solution.py
@@ -11,7 +11,56 @@ from typing import Dict, List
 
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        return 0
+        def color(row: int, col: int, color: int):
+            to_visit = [(row, col)]
+            visited = set()
+
+            while len(to_visit) > 0:
+                plot = to_visit.pop(0)
+
+                p_row, p_col = plot[0], plot[1]
+
+                if not plot in visited:
+                    grid[p_row][p_col] = color
+
+                    if p_row > 0 and grid[p_row - 1][p_col] == 0:
+                        to_visit.append((p_row - 1, p_col))
+
+                    if p_row < len(grid) - 1 and grid[p_row + 1][p_col] == 0:
+                        to_visit.append((p_row + 1, p_col))
+
+                    if p_col < len(grid[p_row]) - 1 and grid[p_row][p_col + 1] == 0:
+                        to_visit.append((p_row, p_col + 1))
+                    
+                    if p_col > 0 and grid[p_row][p_col - 1] == 0:
+                        to_visit.append((p_row, p_col - 1))
+
+                    visited.add(plot)
+
+        island_no = 0
+        
+        height = len(grid)
+        width = len(grid[0])
+
+        for col in range(width):
+            if grid[0][col] == 0:
+                color(0, col, 2)
+            if grid[height - 1][col] == 0:
+                color(height - 1, col, 2)
+
+        for row in range(1, height - 1):
+            if grid[row][0] == 0:
+                color(row, 0, 2)
+            if grid[row][width - 1] == 0:
+                color(row, width - 1, 2)
+
+        for row in range(height):
+            for col in range(width):
+                if grid[row][col] == 0:
+                    island_no -= 1
+                    color(row, col, island_no)
+
+        return abs(island_no)
 
 ###############################################################################
 
@@ -23,6 +72,13 @@ class SolutionTest(unittest.TestCase):
 
         self.assertEqual(
             solution.closedIsland(
+                [[0,1,1,1,0],[1,0,1,0,1],[1,0,1,0,1],[1,0,0,0,1],[0,1,1,1,0]]
+            ),
+            1
+        )
+
+        self.assertEqual(
+            solution.closedIsland(
                 [[1,1,1,1,1,1,1,0],[1,0,0,0,0,1,1,0],[1,0,1,0,1,1,1,0],[1,0,0,0,0,1,0,1],[1,1,1,1,1,1,1,0]]
             ),
             2
@@ -31,7 +87,7 @@ class SolutionTest(unittest.TestCase):
         self.assertEqual(
             solution.closedIsland(
                 [[0,0,1,0,0],[0,1,0,1,0],[0,1,1,1,0]]
-            ), 
+            ),
             1
         )
 
@@ -44,7 +100,7 @@ class SolutionTest(unittest.TestCase):
                 [1,0,1,1,1,0,1],
                 [1,0,0,0,0,0,1],
                 [1,1,1,1,1,1,1]
-            ]), 
+            ]),
             2
         )
 
