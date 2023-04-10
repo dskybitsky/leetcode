c1bd57ca94e564111b652ae173f8f9f6da662dde
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-04-10 10:01:09 +0000

    32 Minor amends

--- src/problems/32-longest-valid-parentheses/solution.py
+++ src/problems/32-longest-valid-parentheses/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        n = len(s)
-
         stack = []
 
         def stack_last(offset = 1):
             return stack[len(stack) - offset] if len(stack) - offset >= 0 else None
+
+        result = 0
 
         for c in s:
             top = stack_last()
@@ -17,7 +17,6 @@ class Solution:
                     if isinstance(top, int):
                         stack.pop()
                         stack.pop()
-
                         size = top
                     else:
                         stack.pop()
@@ -29,17 +28,13 @@ class Solution:
                         size += stack_last()
                         stack.pop()
 
+                    result = max(result, size)
+
                     stack.append(size)
 
                     continue
 
             stack.append(c)
-
-        result = 0
-
-        for item in stack:
-            if isinstance(item, int):
-                result = max(result, item)
 
         return result
 
