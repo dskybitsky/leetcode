b27aa2d0fab2ba43e3ba7c24de59f355b6080eca
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-04-10 09:56:57 +0000

    32 Solved Slow

--- src/problems/32-longest-valid-parentheses/solution.py
+++ src/problems/32-longest-valid-parentheses/solution.py
@@ -1,10 +1,53 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        return 0
+        n = len(s)
+
+        stack = []
+
+        def stack_last(offset = 1):
+            return stack[len(stack) - offset] if len(stack) - offset >= 0 else None
+
+        for c in s:
+            top = stack_last()
+
+            if c == ')':
+                prevTop = stack_last(2)
+
+                if top == '(' or isinstance(top, int) and prevTop == '(':
+                    if isinstance(top, int):
+                        stack.pop()
+                        stack.pop()
+
+                        size = top
+                    else:
+                        stack.pop()
+                        size = 0
+
+                    size += 2
+
+                    while isinstance(stack_last(), int):
+                        size += stack_last()
+                        stack.pop()
+
+                    stack.append(size)
+
+                    continue
+
+            stack.append(c)
+
+        result = 0
+
+        for item in stack:
+            if isinstance(item, int):
+                result = max(result, item)
+
+        return result
 
 if __name__ == '__main__':
     sol = Solution()
 
+    assert sol.longestValidParentheses("()") == 2
+    assert sol.longestValidParentheses(")((()()))((") == 8
     assert sol.longestValidParentheses("(()") == 2
     assert sol.longestValidParentheses(")()())") == 4
     assert sol.longestValidParentheses("") == 0
