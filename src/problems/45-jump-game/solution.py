82ac864f4c11a7b4043e054e58fefbcd0ef03b46
blacksourcer <blacksourcer@wisebits.com>
2023-04-12 13:03:05 +0300

    45 Fast

--- src/problems/45-jump-game/solution.py
+++ src/problems/45-jump-game/solution.py
@@ -3,19 +3,26 @@ from typing import List
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [n for i in range(n)]
 
-        dp[0] = 0
+        result = 0
 
-        for i in range(n):
-            for j in range(i + 1, min(i + nums[i] +  1, n)):
-                dp[j] = min(dp[j], dp[i] + 1)
+        cur_end, cur_far = 0, 0
 
-        return dp[n - 1]
+        for i in range(n - 1):
+            cur_far = max(cur_far, i + nums[i])
+
+            if i == cur_end:
+                result += 1
+                cur_end = cur_far
+
+        return result
 
 if __name__ == '__main__':
     sol = Solution()
 
+    assert sol.jump([2,1,1,1,1]) == 3
+    assert sol.jump([1,1,1,1]) == 3
+    assert sol.jump([1,2,3]) == 2
     assert sol.jump([2,3,1,1,4]) == 2
     assert sol.jump([2,3,0,1,4]) == 2
     
