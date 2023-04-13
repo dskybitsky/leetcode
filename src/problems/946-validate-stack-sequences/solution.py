57ce29450b390e3528f24a2e1c7e618747f5f1e2
blacksourcer <blacksourcer@wisebits.com>
2023-04-13 07:08:34 +0300

    946 Solved

--- src/problems/946-validate-stack-sequences/solution.py
+++ src/problems/946-validate-stack-sequences/solution.py
@@ -2,7 +2,28 @@ from typing import List
 
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        return False
+        n = len(pushed)
+        stack = []
+
+        push_i = 0
+        pop_i = 0
+
+        while push_i < n or pop_i < n:
+            stack_top = None if len(stack) == 0 else stack[-1]
+
+            if pop_i < n and stack_top == popped[pop_i]:
+                stack.pop()
+                pop_i += 1
+                continue
+
+            if push_i < n:
+                stack.append(pushed[push_i])
+                push_i += 1
+                continue
+
+            return False
+
+        return True
 
 if __name__ == '__main__':
     sol = Solution()
