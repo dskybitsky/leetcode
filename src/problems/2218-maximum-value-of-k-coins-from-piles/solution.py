e1173df613fa2de5f07ded3233f02dba5861a035
Skybitsky <dskybitsky@gmail.com>
2023-04-15 02:38:21 -0700

    2218 Slow

--- src/problems/2218-maximum-value-of-k-coins-from-piles/solution.py
+++ src/problems/2218-maximum-value-of-k-coins-from-piles/solution.py
@@ -1,12 +1,46 @@
+from typing import List
+
+
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
-        return 0
+        n = len(piles)
+
+        def solve(piles: List[List[int]], offsets: List[int], k: int) -> int:
+            res = 0
+
+            if k == 0:
+                return 0
+
+            if k == 1:
+                for i in range(n):
+                    if offsets[i] < len(piles[i]):
+                        res = max(res, piles[i][offsets[i]])
+
+                return res
+
+            for i in range(n):
+                if offsets[i] < len(piles[i]):
+                    nextOffsets = offsets.copy()
+                    nextOffsets[i] += 1
+
+                    res = max(res, piles[i][offsets[i]] + solve(piles, nextOffsets, k - 1))
+
+            return res
+
+        offsets = [0] * n
+
+        return solve(piles, offsets, k)
 
 
 if __name__ == '__main__':
     sol = Solution()
 
+    assert sol.maxValueOfCoins(
+        [[37, 88], [51, 64, 65, 20, 95, 30, 26], [9, 62, 20], [44]],
+        9
+    ) == 494
+
     assert sol.maxValueOfCoins([[1, 100, 3], [7, 8, 9]], 2) == 101
     assert sol.maxValueOfCoins([
         [100], [100], [100], [100], [100], [100], [1, 1, 1, 1, 1, 1, 700]
-    ]) == 706
+    ], 7) == 706
