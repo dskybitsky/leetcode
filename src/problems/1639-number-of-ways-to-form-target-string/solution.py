7ea3025ae9a8327bbbbd2f8292437f02f6e56752
Skybitsky <dskybitsky@gmail.com>
2023-04-15 21:46:03 -0700

    1639 Solved Ok

--- src/problems/1639-number-of-ways-to-form-target-string/solution.py
+++ src/problems/1639-number-of-ways-to-form-target-string/solution.py
@@ -3,17 +3,17 @@ from typing import List
 
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
+        def ord0(c: str) -> int:
+            return ord(c) - ord('a')
+
         n = len(target)
         m = len(words[0])
 
-        def countCharOnPosInWords(c: str, pos: int) -> int:
-            res = 0
+        char_cnt = [[0] * 26 for _ in range(m)]
 
-            for i in range(len(words)):
-                if words[i][pos] == c:
-                    res += 1
-
-            return res
+        for i in range(m):
+            for word in words:
+                char_cnt[i][ord0(word[i])] += 1
 
         dp = [[0] * (m + 1) for _ in range(n + 1)]
 
@@ -22,7 +22,7 @@ class Solution:
 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
-                cnt = countCharOnPosInWords(target[i - 1], j - 1)
+                cnt = char_cnt[j - 1][ord0(target[i - 1])]
 
                 dp[i][j] = dp[i - 1][j - 1] * cnt + dp[i][j - 1]
 
