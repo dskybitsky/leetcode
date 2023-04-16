b7419a43c59e8454357fe423f1f8116e2f76d828
Skybitsky <dskybitsky@gmail.com>
2023-04-15 20:31:00 -0700

    70, 746 Solved

---
+++ src/problems/70-climbing-stairs/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * max((n + 1), 3)
+
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+
+if __name__ == '__main__':
+    sol = Solution()
+
+    assert sol.climbStairs(1) == 1
+    assert sol.climbStairs(2) == 2
+    assert sol.climbStairs(3) == 3
