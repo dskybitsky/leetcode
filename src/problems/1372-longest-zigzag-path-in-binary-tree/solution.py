118d86d5402567de31ef3e4ed18b01784386cbe1
blacksourcer <blacksourcer@wisebits.com>
2023-04-19 10:53:10 +0300

    1372 Solved

--- src/problems/1372-longest-zigzag-path-in-binary-tree/solution.py
+++ src/problems/1372-longest-zigzag-path-in-binary-tree/solution.py
@@ -3,29 +3,23 @@ from typing import Optional
 
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        def dfs(root: Optional[TreeNode], depth = -1) -> int:
-            if root is None:
-                return depth
-            
-            leftZz = zigZag(root.left, -1, depth + 1)
-            rightZz = zigZag(root.right, 1, depth + 1)
+        self.ans = 0
 
-            leftDfs = dfs(root.left)
-            rightDfs = dfs(root.right)
-            
-            return max(leftZz, rightZz, leftDfs, rightDfs)
+        def dfs(node: Optional[TreeNode], dir: int, depth = 0) -> int:
+            if node is not None:
+                self.ans = max(self.ans, depth)
 
-        def zigZag(root: Optional[TreeNode], dir, depth = -1) -> int:
-            if root is None:
-                return depth
-            
-            if dir < 0:
-                return zigZag(root.right, 1, depth +1)
-            
-            return zigZag(root.left, -1, depth + 1)
-        
-        return dfs(root)
-        
+                if dir < 0:
+                    dfs(node.left, 1, depth + 1)
+                    dfs(node.right, -1, 1)
+                else:
+                    dfs(node.left, 1, 1)
+                    dfs(node.right, -1, depth + 1)
+
+        dfs(root, -1, 0)
+        dfs(root, 1, 0)
+
+        return self.ans
     
 
 if __name__ == '__main__':
