1c38496cd7ac2c2103980338f56ae19348b07530
Skybitsky <dskybitsky@gmail.com>
2023-04-21 13:16:08 -0700

    Formatting

--- src/problems/1372-longest-zigzag-path-in-binary-tree/solution.py
+++ src/problems/1372-longest-zigzag-path-in-binary-tree/solution.py
@@ -1,11 +1,12 @@
 from tree_node import TreeNode
 from typing import Optional
 
+
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         self.ans = 0
 
-        def dfs(node: Optional[TreeNode], dir: int, depth = 0) -> int:
+        def dfs(node: Optional[TreeNode], dir: int, depth: int = 0) -> int:
             if node is not None:
                 self.ans = max(self.ans, depth)
 
@@ -20,7 +21,7 @@ class Solution:
         dfs(root, 1, 0)
 
         return self.ans
-    
+
 
 if __name__ == '__main__':
     sol = Solution()
