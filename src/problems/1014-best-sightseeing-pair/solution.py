b5487c879236b4ed1391218f83617ff7fc13c6aa
blacksourcer <blacksourcer@wisebits.com>
2023-04-21 06:01:41 +0300

    1014 Solved

--- src/problems/1014-best-sightseeing-pair/solution.py
+++ src/problems/1014-best-sightseeing-pair/solution.py
@@ -3,13 +3,18 @@ from typing import List
 
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        def get_sum(i: int, j: int) -> int:
+            return values[i] + values[j] + i - j
+
         n = len(values)
 
         ans = 0
 
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                ans = max(ans, values[i] + values[j] + i - j)
+        prev_max = values[0]
+
+        for i in range(1, n):
+            ans = max(ans, values[i] - i + prev_max)
+            prev_max = max(prev_max, values[i] + i)
 
         return ans
 
@@ -17,5 +22,7 @@ class Solution:
 if __name__ == '__main__':
     sol = Solution()
 
+    assert sol.maxScoreSightseeingPair([2, 7, 7, 2, 1, 7, 10, 4, 3, 3]) == 16
+    assert sol.maxScoreSightseeingPair([1, 3, 5]) == 7
     assert sol.maxScoreSightseeingPair([8, 1, 5, 2, 6]) == 11
     assert sol.maxScoreSightseeingPair([1, 2]) == 2
