08272248e0df755989845ba62f29e19622606c24
Skybitsky <dskybitsky@gmail.com>
2023-04-21 21:53:56 -0700

    309 Solved

--- src/problems/309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
+++ src/problems/309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
@@ -1,25 +1,33 @@
 from typing import List
+import functools
 
 
 class Solution:
-    def maxProfit(self, prices: List[int], offset: int = 0, min_price: int = -1) -> int:
+    def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
 
-        if offset >= n:
-            return 0
+        @functools.lru_cache(maxsize=None)
+        def solve(offset: int = 0, min_price: int = -1) -> int:
+            if offset >= n:
+                return 0
 
-        if min_price < 0 or prices[offset] < min_price:
-            return self.maxProfit(prices, offset + 1, prices[offset])
+            if min_price < 0 or prices[offset] < min_price:
+                return solve(offset + 1, prices[offset])
 
-        return max(
-            prices[offset] - min_price + self.maxProfit(prices, offset + 2),
-            self.maxProfit(prices, offset + 1, min_price)
-        )
+            res = max(
+                prices[offset] - min_price + solve(offset + 2),
+                solve(offset + 1, min_price)
+            )
+
+            return res
+
+        return solve()
 
 
 if __name__ == '__main__':
     sol = Solution()
 
+    assert sol.maxProfit([1, 2, 4, 2, 5, 7, 2, 4, 9, 0]) == 11
     assert sol.maxProfit([6, 1, 3, 2, 4, 7]) == 6
     assert sol.maxProfit([1, 2, 3]) == 2
     assert sol.maxProfit([1, 2, 3, 0, 2]) == 3
