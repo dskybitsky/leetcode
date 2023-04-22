d6cd935a70fda60706ac99f8d247d8c8b6e18e4b
Skybitsky <dskybitsky@gmail.com>
2023-04-21 21:38:51 -0700

    309 Slow 2

--- src/problems/309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
+++ src/problems/309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
@@ -2,16 +2,16 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int], offset: int = 0, min_price: int = -1, sum: int = 0) -> int:
+    def maxProfit(self, prices: List[int], offset: int = 0, min_price: int = -1) -> int:
         n = len(prices)
 
         if offset >= n:
-            return sum
+            return 0
 
         if min_price < 0 or prices[offset] < min_price:
-            return self.maxProfit(prices, offset + 1, prices[offset], sum)
+            return self.maxProfit(prices, offset + 1, prices[offset])
 
-        return sum + max(
+        return max(
             prices[offset] - min_price + self.maxProfit(prices, offset + 2),
             self.maxProfit(prices, offset + 1, min_price)
         )
@@ -20,6 +20,7 @@ class Solution:
 if __name__ == '__main__':
     sol = Solution()
 
+    assert sol.maxProfit([6, 1, 3, 2, 4, 7]) == 6
     assert sol.maxProfit([1, 2, 3]) == 2
     assert sol.maxProfit([1, 2, 3, 0, 2]) == 3
     assert sol.maxProfit([1]) == 0
