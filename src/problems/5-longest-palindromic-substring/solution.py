03dd963de21bc3b2a10baa2b27a8ca9fcca9098e
blacksourcer <blacksourcer@wisebits.com>
2023-04-26 17:04:15 +0300

    5 Slow

--- src/problems/5-longest-palindromic-substring/solution.py
+++ src/problems/5-longest-palindromic-substring/solution.py
@@ -2,35 +2,34 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
 
-        dp = [0] * n
+        memo = {}
 
-        dp[0] = 1
-        dp[1] = 2 if s[0] == s[1] else 1
-
-        p_len = 2 if s[0] == s[1] else 1
-        p_start = 0 if s[0] == s[1] else 1
-
-        for i in range(2, n):
-            if s[i] == s[i - dp[i - 1]]:
-                dp[i] = dp[i - 1] + 1
-            elif s[i] == s[i - dp[i - 1] - 1]:
-                dp[i] = dp[i - 1 - 1] + 1
-            else:
-                dp[i] = 1
-
-            if dp[i] > p_len:
-                p_len = dp[i]
-                p_start = i - p_len + 1
+        def is_palindrome(s: str):
+            if s not in memo:
+                memo[s] = s == s[::-1]
             
-        return s[p_start:p_start + p_len]
+            return memo[s]
+
+
+        ans = ""
+
+        for i in range(n):
+            for j in range(i, n):
+                s1 = s[i:j + 1]
+
+                if is_palindrome(s1) and len(s1) > len(ans):
+                    ans = s1
+
+        return ans
+            
 
 
 if __name__ == '__main__':
     sol = Solution()
 
-    assert sol.longestPalindrome("aba") == "aba"
     assert sol.longestPalindrome("aacabdkacaa") == "aca"
-    assert sol.longestPalindrome("babad") == "aba"
+    assert sol.longestPalindrome("aba") == "aba"
+    assert sol.longestPalindrome("babad") == "bab"
     assert sol.longestPalindrome("cbbd") == "bb"
 
 
