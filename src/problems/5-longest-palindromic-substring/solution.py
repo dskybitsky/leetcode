45dab74241626f77628ab5784bca0146e26d8933
blacksourcer <blacksourcer@wisebits.com>
2023-04-27 14:20:56 +0300

    5 Solved

--- src/problems/5-longest-palindromic-substring/solution.py
+++ src/problems/5-longest-palindromic-substring/solution.py
@@ -2,40 +2,38 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
 
-        memo = {}
+        dp = [[0] * n for _ in range(n)]
 
-        def is_palindrome(s: str):
-            if s not in memo:
-                memo[s] = s == s[::-1]
-            
-            return memo[s]
+        start = 0
+        length = 1
 
+        for d in range(n):
+            for offset in range(n - d):
+                i = offset
+                j = d + offset
+                if (
+                    s[i] == s[j]
+                    and (
+                        j - i < 2
+                        or dp[i + 1][j - 1] == 1
+                    )
+                ):
+                    dp[i][j] = 1
 
-        ans = ""
-        ans_len = 0
+                    if j - i + 1 > length:
+                        length = j - i + 1
+                        start = i
 
-        for i in range(n):
-            for j in range(i, n):
-
-                curr_len = j - i + 1
-
-                if curr_len > ans_len:
-                    s1 = s[i:j + 1]
-
-                    if is_palindrome(s1):
-                        ans = s1
-                        ans_len = curr_len
-
-        return ans
+        return s[start:start + length]
             
 
 
 if __name__ == '__main__':
     sol = Solution()
 
+    assert sol.longestPalindrome("babad") == "bab"
     assert sol.longestPalindrome("aacabdkacaa") == "aca"
     assert sol.longestPalindrome("aba") == "aba"
-    assert sol.longestPalindrome("babad") == "bab"
     assert sol.longestPalindrome("cbbd") == "bb"
 
 
