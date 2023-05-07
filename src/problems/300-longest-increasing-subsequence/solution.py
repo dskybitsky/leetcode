dc623df0a7ed9d3239230984509635afbee0501b
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-05-07 15:52:28 +0300

    300 Improved

--- src/problems/300-longest-increasing-subsequence/solution.py
+++ src/problems/300-longest-increasing-subsequence/solution.py
@@ -6,29 +6,27 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
 
-        inf = 10001
+        lis = []
 
-        dp = [inf] * (n + 1)
+        for num in nums:
+            if len(lis) == 0 or lis[-1] < num:
+                lis.append(num)
+            else:
+                idx = bisect.bisect_left(lis, num)
 
-        dp[0] = -inf
+                lis[idx] = num                
 
-        ans = 0
-
-        for i in range(n):
-            l = bisect.bisect_right(dp, nums[i])
-
-            if dp[l - 1] < nums[i] and nums[i] < dp[l]:
-                dp[l] = nums[i]
-                ans = max(ans, l)
-
-        return ans
+        return len(lis)
 
 
 if __name__ == "__main__":
     sol = Solution()
 
+    assert sol.lengthOfLIS([4, 10, 4, 3, 8, 9]) == 3
     assert sol.lengthOfLIS([0]) == 1
     assert sol.lengthOfLIS([1, 3, 6, 7, 9, 4, 10, 5, 6]) == 6
     assert sol.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
     assert sol.lengthOfLIS([0, 1, 0, 3, 2, 3]) == 4
     assert sol.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]) == 1
+
+    print("OK")
