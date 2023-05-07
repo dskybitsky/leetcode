8e4de6ea617df25aac461f47d0fc4aae80764f0f
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-05-07 10:32:09 +0300

    1964 Solved

--- src/problems/1964-find-the-longest-valid-obstacle-course-at-each-position/solution.py
+++ src/problems/1964-find-the-longest-valid-obstacle-course-at-each-position/solution.py
@@ -1,10 +1,28 @@
 from typing import List
+import bisect
 
 
 class Solution:
     def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
-        return []
-    
+        n = len(obstacles)
+
+        inf = 10000001
+
+        ans = [1] * n
+        lis = []
+
+        for i in range(n):
+            idx = bisect.bisect_right(lis, obstacles[i])
+
+            if idx == len(lis):
+                lis.append(obstacles[i])
+            else:
+                lis[idx] = obstacles[i]
+            
+            ans[i] = idx + 1
+
+        return ans
+
 
 if __name__ == "__main__":
     sol = Solution()
