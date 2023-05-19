14e93087527ed440a7acc1fae26f03980e46eb0b
blacksourcer <blacksourcer@wisebits.com>
2023-05-19 17:00:41 +0300

    1027 Partial

--- src/problems/1027-longest-arithmetic-subsequence/main.py
+++ src/problems/1027-longest-arithmetic-subsequence/main.py
@@ -2,7 +2,28 @@ from typing import List
 
 class Solution:
     def longestArithSeqLength(self, nums: List[int]) -> int:
-        return 0
+        dp = { }
+
+        ans = 0
+
+        n = len(nums)
+
+        for i in range(n):
+            dp[0] = { nums[i]: 1 }
+
+            for j in range(i):
+                diff = nums[i] - nums[j]
+
+                if diff not in dp:
+                    dp[diff] = { nums[i]: 2}
+                elif nums[j] in dp[diff]:
+                    dp[diff][nums[i]] = dp[diff][nums[j]] + 1
+                else:
+                    dp[diff][nums[i]] = 1
+
+                ans = max(ans, dp[diff][nums[i]])
+        
+        return ans
     
 
 sol = Solution()
