8740b06e9b31b2ae4969f4ae0f1c793a357ff2ca
blacksourcer <blacksourcer@wisebits.com>
2023-05-19 16:29:59 +0300

    1027 Basis

---
+++ src/problems/1027-longest-arithmetic-subsequence/main.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        return 0
+    
+
+sol = Solution()
+
+assert sol.longestArithSeqLength([3, 6, 9, 12]) == 4
+assert sol.longestArithSeqLength([9, 4, 7, 2, 10]) == 3
+assert sol.longestArithSeqLength([20, 1, 15, 3, 10, 5, 8]) == 4
