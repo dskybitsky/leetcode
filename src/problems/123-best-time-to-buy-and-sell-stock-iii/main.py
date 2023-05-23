316cbc42cc844e9e44cb305fdead540856b4e8cb
blacksourcer <blacksourcer@wisebits.com>
2023-05-23 18:11:06 +0300

    123 Solved

--- src/problems/123-best-time-to-buy-and-sell-stock-iii/main.py
+++ src/problems/123-best-time-to-buy-and-sell-stock-iii/main.py
@@ -5,34 +5,31 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
 
-        if n < 2:
-            return 0
+        dp_1 = [0] * n
+
+        min_price = prices[0]
+
+        for i in range(1, n):
+            dp_1[i] = max(dp_1[i - 1], prices[i] - min_price)
+            min_price = min(min_price, prices[i])
+
+        dp_2 = [0] * n
+
+        max_price = prices[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            dp_2[i] = max(dp_2[i + 1], max_price - prices[i])
+            max_price = max(max_price, prices[i])
         
         ans = 0
-        
-        for i in range(1, n):
-            l_ans = 0
-            min_price = prices[0]
 
-            for j in range(i + 1):
-                l_ans = max(l_ans, prices[j] - min_price)
-                min_price = min(min_price, prices[j])
-            
-            r_ans = 0
-
-            if i < n - 1:
-                min_price = prices[i + 1]
-
-                for j in range(i + 1, n):
-                    r_ans = max(r_ans, prices[j] - min_price)
-                    min_price = min(min_price, prices[j])
-
-            ans = max(ans, l_ans + r_ans)
+        for i in range(n):
+            ans = max(ans, dp_1[i] + dp_2[i])
 
         return ans
     
 
 sol = Solution()
 
-assert sol.maxProfit([1,2,3,4,5]) == 4
 assert sol.maxProfit([3,3,5,0,0,3,1,4]) == 6
+assert sol.maxProfit([1,2,3,4,5]) == 4
