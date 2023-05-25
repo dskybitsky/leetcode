90ecf354289103b09230bd16b08a1cfa8b47a0ae
blacksourcer <blacksourcer@wisebits.com>
2023-05-25 07:36:11 +0300

    837 Solved

---
+++ src/problems/837-new-21-game/main.py
@@ -0,0 +1,26 @@
+import math
+
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        s = 1 if k> 0 else 0
+
+        for i in range(1, n + 1):
+            dp[i] = s / maxPts
+            
+            if i < k:
+                s += dp[i]
+            
+            if i - maxPts >= 0 and i - maxPts < k:
+                s -= dp[i - maxPts]
+
+        return sum(dp[k:])
+
+
+sol = Solution()
+
+assert round(sol.new21Game(n = 10, k = 1, maxPts = 10), 5) == 1.0
+assert round(sol.new21Game(n = 6, k = 1, maxPts = 10), 5) == 0.6
+assert round(sol.new21Game(n = 21, k = 17, maxPts = 10), 5) == 0.73278
