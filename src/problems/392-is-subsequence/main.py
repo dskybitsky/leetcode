0b5f50bc531f10e43ee259ff4277e3548193fa25
blacksourcer <blacksourcer@wisebits.com>
2023-06-07 14:42:33 +0300

    392 Solved (faster)

--- src/problems/392-is-subsequence/main.py
+++ src/problems/392-is-subsequence/main.py
@@ -1,21 +1,35 @@
+import bisect
+
+
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         m = len(s)
         n = len(t)
 
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        th = { }
 
-        for i in range(n + 1):
-            dp[0][i] = 1
+        for i in range(n):
+            if t[i] not in th:
+                th[t[i]] = [i]
+            else:
+                th[t[i]].append(i)
 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if s[i - 1] == t[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
-                else:
-                    dp[i][j] = dp[i][j - 1]
+        min_idx = 0
 
-        return dp[m][n] == 1
+        for i in range(m):
+            c = s[i]
+
+            if c not in th:
+                return False
+            
+            pos = bisect.bisect_left(th[c], min_idx)
+
+            if pos == len(th[c]):
+                return False
+            
+            min_idx = th[c][pos] + 1
+        
+        return True
 
 
 sol = Solution()
