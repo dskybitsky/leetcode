ca911c6f3b5519177d9d59ba320bbf5239abe1b1
blacksourcer <blacksourcer@wisebits.com>
2023-06-21 09:13:06 +0300

    2448 Solved

--- src/problems/2448-minimum-cost-to-make-array-equal/main.py
+++ src/problems/2448-minimum-cost-to-make-array-equal/main.py
@@ -2,18 +2,31 @@ from typing import List
 
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        n = len(nums)
+        num_and_cost = sorted([num, c] for num, c in zip(nums, cost))
+        n = len(cost)
 
-        res = None
+        prefix_cost = [0] * n
+        prefix_cost[0] = num_and_cost[0][1]
 
-        for i in range(n):
-            s = 0
-            for j in range(n):
-                s += abs(nums[i] - nums[j]) * cost[j]
+        for i in range(1, n):
+            prefix_cost[i] = num_and_cost[i][1] + prefix_cost[i - 1]
+        
+        total_cost = 0
 
-            res = s if res is None else min(res, s)
+        for i in range(1, n):
+            total_cost += num_and_cost[i][1] * (num_and_cost[i][0] - num_and_cost[0][0])
 
-        return res
+        answer = total_cost
+
+        for i in range(1, n):
+            gap = num_and_cost[i][0] - num_and_cost[i - 1][0]
+
+            total_cost += prefix_cost[i - 1] * gap
+            total_cost -= gap * (prefix_cost[n - 1] - prefix_cost[i - 1])
+
+            answer = min(answer, total_cost)
+
+        return answer
     
 
 sol = Solution()
