328bdb01b6802cfd5493519640b5928439acbf60
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-07-04 22:01:05 +0300

    503 Basis

---
+++ src/problems/503-next-greater-element-ii/main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        return []
+    
+
+sol = Solution()
+
+assert sol.nextGreaterElement([1,2,1]) == [2, -1, 2]
