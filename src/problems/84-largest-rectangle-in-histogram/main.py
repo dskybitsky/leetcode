ac248174319691c4cae21615e8db311adec29a46
dskybitsky <126291913+dskybitsky@users.noreply.github.com>
2023-07-04 21:22:28 +0300

    84 Draft

--- src/problems/84-largest-rectangle-in-histogram/main.py
+++ src/problems/84-largest-rectangle-in-histogram/main.py
@@ -5,24 +5,27 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
 
-        @cache
-        def solve(i1: int = 0, i2: int = n - 1) -> int:
-            if i1 > i2:
-                return 0
-            
-            sq = (i2 - i1 + 1) * mins[i1][i2]
-
-            return max(sq, solve(i1 + 1, i2), solve(i1, i2 - 1))
-        
-        mins = [[None] * n for _ in range(n)]
+        res = 0
 
         for i1 in range(n):
+            
+            min_h = heights[i1]
+
             for i2 in range(i1, n):
-                mins[i1][i2] = min(mins[i1][i2 - 1], heights[i2]) if i2 > i1 else heights[i2]
-        
-        return solve()
+                min_h = min(min_h, heights[i2])
+
+                sq = (i2 - i1 + 1) * min_h
+
+                res = max(res, sq)
+
+        return res
+
 
 
 sol = Solution()
 
+assert sol.largestRectangleArea([4,2,0,3,2,5]) == 6
+assert sol.largestRectangleArea([1,1]) == 2
 assert sol.largestRectangleArea([2,1,5,6,2,3]) == 10
+
+print("Ok")
