4f3277b16a74ee3ecf972fc9655bd759b00944bc
blacksourcer <blacksourcer@wisebits.com>
2023-07-06 11:37:42 +0300

    84 Solved

--- src/problems/84-largest-rectangle-in-histogram/main.py
+++ src/problems/84-largest-rectangle-in-histogram/main.py
@@ -2,24 +2,43 @@ from typing import List
 from functools import cache
 
 class Solution:
+    def lesserElements(self, nums: List[int], dir: int) -> List[int]:
+        n = len(nums)
+
+        stack = []
+
+        res = [None] * n
+
+        dir_range = range(n, -2, -1) if dir > 0 else range(-1, n + 1)
+
+        for i in dir_range:
+            num = nums[i] if i >= 0 and i < n else 0
+            
+            while len(stack) and num <= (nums[stack[0]] if stack[0] >= 0 and stack[0] < n else 0):
+                stack.pop(0)
+
+            if i >= 0 and i < n:
+                res[i] = stack[0] if len(stack) else None
+
+            stack.insert(0, i)
+
+        return res
+    
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
 
         res = 0
 
-        for i1 in range(n):
-            
-            min_h = heights[i1]
+        prevs = self.lesserElements(heights, -1)
+        nexts = self.lesserElements(heights, 1)
 
-            for i2 in range(i1, n):
-                min_h = min(min_h, heights[i2])
+        for i in range(n):
+            prev = prevs[i] if prevs[i] is not None else i - 1
+            next = nexts[i] if nexts[i] is not None else i + 1
 
-                sq = (i2 - i1 + 1) * min_h
-
-                res = max(res, sq)
+            res = max(res, heights[i] * (next - prev - 1))
 
         return res
-
 
 
 sol = Solution()
