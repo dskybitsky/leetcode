417c09c78bfe21cd7f3f382ef7b97d74f421248e
blacksourcer <blacksourcer@wisebits.com>
2023-07-06 06:16:40 +0300

    503 Solved

--- src/problems/503-next-greater-element-ii/main.py
+++ src/problems/503-next-greater-element-ii/main.py
@@ -7,38 +7,24 @@ class Solution:
 
         stack = []
 
-        res = [None] * n
-        p = 0
+        res = [0] * n
 
-        for num in nums:
-            if len(stack) == 0:
-                stack.insert(0, num)
-                continue
+        for _ in [0, 1]:
+            for i in range(n - 1, -1, -1):
+                while len(stack) and nums[i] >= nums[stack[0]]:
+                    stack.pop(0)
 
-            while len(stack) and num > stack[0]:
-                res[p] = num
-                p += 1
-                stack.pop(0)
-                
-            stack.insert(0, num)
-        
-        i = 0
+                res[i] = nums[stack[0]] if len(stack) else -1
 
-        m = len(stack)
-
-        for s in range(m):
-            p1 = n - s - 1
-
-            while i < n and nums[i] <= stack[s]:
-                i += 1
-
-            res[p1] = nums[i] if i < n else -1
+                stack.insert(0, i)
 
         return res
     
 
 sol = Solution()
 
+assert sol.nextGreaterElements([3, 8, 4, 1, 2]) == [8, -1, 8, 2, 3]
+assert sol.nextGreaterElements([1,2,3,4,5,6,5,4,5,1,2,3]) == [2,3,4,5,6,-1,6,5,6,2,3,4]
 assert sol.nextGreaterElements([1,2,3,2,1]) == [2,3,-1,3,2]
 assert sol.nextGreaterElements([5,4,3,2,1]) == [-1,5,5,5,5]
 assert sol.nextGreaterElements([1,1,1,1,1]) == [-1,-1,-1,-1,-1]
