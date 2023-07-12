c07345174eaab7c0d5c9c6cdb468ac46b7195d64
dskybitsky <dskybitsky@gmail.com>
2023-07-12 10:13:00 +0300

    94 Solved (iter)

--- src/problems/94-binary-tree-inorder-traversal/main.py
+++ src/problems/94-binary-tree-inorder-traversal/main.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+import heapq
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -11,13 +12,55 @@ class Solution:
         if root is None:
             return []
         
-        left = self.inorderTraversal(root.left)
-        right = self.inorderTraversal(root.right)
+        h = [(0, root)]
 
-        return left + [root.val] + right
+        to_visit = [(0, 100, root)]
+
+        while len(to_visit):
+            w, d, node = to_visit.pop(0)
+
+            d1 = d / 2
+
+            if node.left:
+                h.append((w - d1, node.left))
+                to_visit.append((w - d1, d1, node.left))
+            
+            if node.right:
+                h.append((w + d1, node.right))
+                to_visit.append((w + d1, d1, node.right))
+
+        h.sort(key = lambda x: x[0])
+
+        n = len(h)
+
+        res = [0] * n
+        
+        for i in range(n):
+            res[i] = h[i][1].val
+
+        return res
 
 
 sol = Solution()
+
+assert sol.inorderTraversal(
+    TreeNode(
+        3,
+        TreeNode(
+            1,
+            None,
+            TreeNode(2)
+        ),
+    )
+) == [1, 2, 3]
+
+assert sol.inorderTraversal(
+    TreeNode(
+        3,
+        TreeNode(1),
+        TreeNode(2)
+    )
+) == [1, 3, 2]
 
 assert sol.inorderTraversal(
     TreeNode(
